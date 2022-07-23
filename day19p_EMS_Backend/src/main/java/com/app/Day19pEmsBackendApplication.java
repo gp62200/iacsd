0683373140541aef6f760d6b951b3ae98e724d71
GaneshPatil <68843402+gp62200@users.noreply.github.com>
2022-07-23 20:14:34 +0530

    Add files via upload

---
+++ day19p_EMS_Backend/src/main/java/com/app/Day19pEmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day19pEmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day19pEmsBackendApplication.class, args);
+	}
+
+}
