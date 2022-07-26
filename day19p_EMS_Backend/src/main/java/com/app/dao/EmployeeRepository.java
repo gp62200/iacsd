6a025bdc25858ae284f164d32473aaae1b19d847
GaneshPatil <68843402+gp62200@users.noreply.github.com>
2022-07-26 17:59:39 +0530

    Delete day19p_EMS_Backend directory

--- day19p_EMS_Backend/src/main/java/com/app/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
